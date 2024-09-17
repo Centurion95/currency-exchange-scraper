e8200894e11cd501bb1d2e2bce194eb4346528e9
Rodrigo Centurion <rodrigo@debian.centurion>
2024-09-17 00:20:51 -0400

    scraper improved

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,17 +1,37 @@
 import { Builder, By, until } from 'selenium-webdriver';
 import { WEB_URL_CAMBIOSCHACO } from './constants';
 
-export async function getExchangeRates(): Promise<{ exchangeRate: string }> {
+export async function getExchangeRates(): Promise<{ currency: string; buy: string; sell: string }[]> {
+
   let driver = await new Builder().forBrowser('chrome').build();
-  let exchangeRate: string;
 
   try {
     await driver.get(WEB_URL_CAMBIOSCHACO);
 
-    const element = await driver.wait(until.elementLocated(By.xpath('/html/body/div[2]/section/div[2]/div[1]/div[2]/div[5]/table/tbody/tr[1]/td[2]/span')), 10000);
-    exchangeRate = await element.getText();
+    // Espera a que la tabla esté cargada
+    await driver.wait(until.elementLocated(By.xpath('//table[@class="table table-hover cotiz-tabla"]')), 10000);
 
-    return { exchangeRate };
+    // Obtiene las filas de las primeras 4 monedas
+    let currencyRows = await driver.findElements(By.xpath('//tbody[@id="main-exchange-content"]/tr[position() <= 4]'));
+
+    // Inicializa el array para las monedas
+    let currencies = [];
+
+    // Itera sobre las filas y extrae la información
+    for (let row of currencyRows) {
+      let name = await row.findElement(By.xpath('.//td[1]/a')).getText();
+      let buyValue = await row.findElement(By.xpath('.//td[2]/span')).getText();
+      let sellValue = await row.findElement(By.xpath('.//td[3]/span')).getText();
+
+      // Crea un objeto para la moneda y añádelo al array
+      currencies.push({
+        currency: name,
+        buy: buyValue,
+        sell: sellValue
+      });
+    }
+
+    return currencies;
   } finally {
     await driver.quit();
   }
